d762ab4e19df32f8df9ca00dec5d228ae066e634
yhy <18989893637@189.cn>
2017-02-21 19:23:07 +0800

    去掉了 安装 gcc, g++.因为自动安装的 gcc,g++ 可能不符合要求

--- poco.sh
+++ poco.sh
@@ -9,13 +9,11 @@ echo "安装 poco"
 #安装poco 1.6.0 依赖
 if [ $g_system == "centos" ]; then
 	yum install make -y
-	yum install g++ -y
 	yum install openssl-devel -y
 	yum install unixODBC-devel -y
 	yum install mysql-devel -y
 elif [ $g_system == "ubuntu" ]; then
 	apt-get install make  -y
-	apt-get install g++  -y
 	apt-get install openssl -y
 	apt-get install libssl-dev -y
 	apt-get install unixodbc -y
