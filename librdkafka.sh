0dc4733471d8ff09a5c072d788986c0beea27308
徐佳乐 <254525337@qq.com>
2017-07-09 14:14:39 +0800

    modify librdkafka.sh

--- librdkafka.sh
+++ librdkafka.sh
@@ -4,6 +4,20 @@ source ./common.sh
 
 
 echo "安装 librdkafka"
-
+#安装poco 1.6.0 依赖
+if [ $g_system == "centos" ]; then
+        yum install make -y
+        yum install openssl-devel -y
+        yum install cyrus-sasl -y
+        yum install cyrus-sasl-devel -y
+elif [ $g_system == "ubuntu" ]; then
+        apt-get install make  -y
+        apt-get install openssl -y
+        apt-get install libssl-dev -y
+        apt-get install libsasl2-dev -y
+        apt-get install python-dev -y
+else
+        echo "未识别系统"
+fi
 
 install_cpp "${g_current_path}/librdkafka" "kafka" "v0.11.0" "librdkafka"
